cfef64affec7d694639e4ffc11da0fb7eae965cf
kaizhu94 <zhukai0803@gmail.com>
2020-11-18 14:14:41 -0500

    Add Phase I

--- chess_project/board.rb
+++ chess_project/board.rb
@@ -2,32 +2,37 @@ require_relative "piece.rb"
 class Board
 
     def initialize
-        @board = Array.new(8){Array.new(8, Piece.new)}
+        @board = Array.new(8){Array.new(8, Piece.new("_"))}
         # @board[1].each { |ele| ele = :X }#Piece.new}
         @board.each_with_index do |row, row_idx|
             row.each_with_index do |ele, col_idx|
                 # @board[row_idx][col_idx] = :X
             end
         end
-        p @board
+        # p @board
 
-        rook = Piece.new(:Rook)
-        knight = Piece.new(:Knight)
-        bishop = Piece.new(:Bishop)
-        king =  Piece.new(:King)
-        queen = Piece.new(:Queen)
-        pawn = Piece.new(:Pawn)
-        null = Piece.new("_")
-        @board[0] = [rook, knight, bishop, king, queen, bishop, knight, rook]
-        @board[7] = [rook, knight, bishop, king, queen, bishop, knight, rook]
-        @board[1] = [pawn] * 8
-        @board[6] = [pawn] * 8
-        (2..5).each do |row|
-            @board[row] = [null] * 8
-        end
-
+        # rook = Piece.new("♖")
+        # knight = Piece.new("♘")
+        # bishop = Piece.new("♗")
+        # king =  Piece.new("♔")
+        # queen = Piece.new('♛')
+        # pawn = Piece.new("♙")
+        # null = Piece.new("_")
+         
+        # @board[0] = [rook, knight, bishop, king, queen, bishop, knight, rook]
+        # @board[7] = [rook, knight, bishop, king, queen, bishop, knight, rook]
+        # @board[1] = [pawn] * 8
+        # @board[6] = [pawn] * 8
+        # (2..5).each do |row|
+        #     @board[row] = [null] * 8
+        # end
+        render
     end
 
+    def render 
+        @board.each{|row| puts row.join(" ")}
+    end
+    
     def [](pos) #[0,1]
         row, col = pos 
         @board[row][col]
@@ -39,11 +44,13 @@ class Board
     end
 
     def move_piece(color, start_pos, end_pos)
-        if @board[start_pos].color == color && @board[end_pos].color == "_"
-            @board[end_pos] = @board[start_pos]
-            @board[start_pos] = Piece.new("_")
+        if  color == self[start_pos].color && self[end_pos].color == "_"
+            self[end_pos] = self[start_pos]
+            self[start_pos] = Piece.new("_")
+        else
+            raise "Not Valid Move!"
         end
-        p @board
+        render
     end
 
     def valid_pos?(pos)
